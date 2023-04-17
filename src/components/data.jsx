f7770da242757962788d8b70495a5d45c6b58cb6
Anurag Kamboj <anurag.kamboj.mat21@itbhu.ac.in>
2023-04-17 16:47:52 +0530

    made some changes

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,35 +1,44 @@
-import Woman1 from "../images/woman1.png"
-import Woman2 from "../images/woman2.png"
-import Woman3 from "../images/woman3.png"
-import c1 from "../images/c1.jpg"
-import c2 from "../images/c2.jpg"
-import c3 from "../images/c3.jpg"
-import hoodie1 from "../images/hoodie1.jpg"
-import hoodie2 from "../images/hoodie2.jpg"
-import hoodie3 from "../images/hoodie3.jpg"
-import hoodie4 from "../images/hoodie4.jpg"
-import hoodie5 from "../images/hoodie5.jpg"
+// import Woman1 from "../images/woman1.png"
+// import Woman2 from "../images/woman2.png"
+// import Woman3 from "../images/woman3.png"
+// import c1 from "../images/c1.jpg"
+// import c2 from "../images/c2.jpg"
+// import c3 from "../images/c3.jpg"
+// import hoodie1 from "../images/hoodie1.jpg"
+// import hoodie2 from "../images/hoodie2.jpg"
+// import hoodie3 from "../images/hoodie3.jpg"
+// import hoodie4 from "../images/hoodie4.jpg"
+// import hoodie5 from "../images/hoodie5.jpg"
+
+import r1 from "../images/R1.jpeg"
+import r2 from "../images/r2.jpeg"
+import r3 from "../images/r3.jpeg"
+import r4 from "../images/r4.jpeg"
+import r5 from "../images/r5.jpeg"
+import r6 from "../images/r6.jpeg"
+import r8 from "../images/r8.jpeg"
+
 
 
 
 const sliderItems = [
     {
         id: 1,
-        img: Woman1,
+        img: r2,
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
         bg: "f5fafd",
     },
     {
         id: 2,
-        img: Woman2,
+        img: r1,
         title: "AUTUMN COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
         bg: "fcf1ed",
     },
     {
         id: 3,
-        img: Woman3,
+        img: r3,
         title: "WINTER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
         bg: "f5fafd",
@@ -39,17 +48,17 @@ const sliderItems = [
 const categories = [
     {
         id: 1,
-        img: c1,
+        img: r4,
         title: "SHIRT STYLE"
     },
     {
         id: 2,
-        img: c2,
+        img: r5,
         title: "LOUNGEWEAR LOVE"
     },
     {
         id: 1,
-        img: c3,
+        img: r6,
         title: "LIGHT JACKETS"
     },
 ]
@@ -57,39 +66,119 @@ const categories = [
 const popularProducts = [
     {
         id: 1,
-        img : hoodie1,
+        img : r8,
     },
     {
         id: 2,
-        img : hoodie2,
+        img : r1,
     },
     {
         id: 3,
-        img : hoodie3,
+        img : r2,
     },
     {
         id: 4,
-        img : hoodie4,
+        img : r3,
     },
     {
         id: 5,
-        img : hoodie5,
+        img : r4,
     },
     {
         id: 6,
-        img : hoodie5,
+        img : r5,
     },
     {
         id: 7,
-        img : hoodie5,
+        img : r6,
     },
     {
         id: 8,
-        img : hoodie5,
+        img : r8,
     },
 ]
 
-
-
 export default sliderItems;
 export { categories,popularProducts};
+
+// const sliderItems = [
+//     {
+//         id: 1,
+//         img: Woman1,
+//         title: "SUMMER SALE",
+//         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+//         bg: "f5fafd",
+//     },
+//     {
+//         id: 2,
+//         img: Woman2,
+//         title: "AUTUMN COLLECTION",
+//         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+//         bg: "fcf1ed",
+//     },
+//     {
+//         id: 3,
+//         img: Woman3,
+//         title: "WINTER SALE",
+//         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+//         bg: "f5fafd",
+//     },
+// ]
+
+// const categories = [
+//     {
+//         id: 1,
+//         img: c1,
+//         title: "SHIRT STYLE"
+//     },
+//     {
+//         id: 2,
+//         img: c2,
+//         title: "LOUNGEWEAR LOVE"
+//     },
+//     {
+//         id: 1,
+//         img: c3,
+//         title: "LIGHT JACKETS"
+//     },
+// ]
+
+// const popularProducts = [
+//     {
+//         id: 1,
+//         img : hoodie1,
+//     },
+//     {
+//         id: 2,
+//         img : hoodie2,
+//     },
+//     {
+//         id: 3,
+//         img : hoodie3,
+//     },
+//     {
+//         id: 4,
+//         img : hoodie4,
+//     },
+//     {
+//         id: 5,
+//         img : hoodie5,
+//     },
+//     {
+//         id: 6,
+//         img : hoodie5,
+//     },
+//     {
+//         id: 7,
+//         img : hoodie5,
+//     },
+//     {
+//         id: 8,
+//         img : hoodie5,
+//     },
+// ]
+
+
+
+// export default sliderItems;
+// export { categories,popularProducts};
